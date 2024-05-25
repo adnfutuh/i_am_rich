509b5d5109ed7f23f081b6306189bd03e765409d
rizki <rizkiadnanfutuh@gmail.com>
2024-05-25 23:28:13 +0700

    use the safe area

--- i_am_rich_app/lib/main.dart
+++ i_am_rich_app/lib/main.dart
@@ -3,19 +3,21 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.blueGrey,
-        appBar: AppBar(
-          title: const Text(
-            'i am rich',
+      home: SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.blueGrey,
+          appBar: AppBar(
+            title: const Text(
+              'i am rich',
+            ),
+            centerTitle: true,
+            backgroundColor: Colors.blueGrey[900],
           ),
-          centerTitle: true,
-          backgroundColor: Colors.blueGrey[900],
-        ),
-        body: const Center(
-          child: Image(
-            image: AssetImage(
-              'assets/images/diamond.png',
+          body: const Center(
+            child: Image(
+              image: AssetImage(
+                'assets/images/diamond.png',
+              ),
             ),
           ),
         ),
